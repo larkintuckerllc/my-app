dcde346f1dfedd655cea07dd3f0d60cafa42b98b
John Tucker <jtucker@appirio.com>
2019-06-01 15:53:20 -0400

    Iteration

---
+++ src/modules/my/iteration/iteration.js
@@ -0,0 +1,21 @@
+import { LightningElement } from 'lwc';
+
+export default class Iteration extends LightningElement {
+    contacts = [
+        {
+            id: 1,
+            name: 'Amy Taylor',
+            title: 'VP of Engineering'
+        },
+        {
+            id: 2,
+            name: 'Michael Jones',
+            title: 'VP of Sales'
+        },
+        {
+            id: 3,
+            name: 'Jennifer Wu',
+            title: 'CEO'
+        }
+    ];
+}
