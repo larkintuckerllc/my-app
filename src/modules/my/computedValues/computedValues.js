3fc6c047762c5b53571ad79999843dda29505a73
John Tucker <jtucker@appirio.com>
2019-06-01 15:31:57 -0400

    Fix

--- src/modules/my/computedValues/computedValues.js
+++ src/modules/my/computedValues/computedValues.js
@@ -1,6 +1,6 @@
 import { LightningElement, track } from 'lwc';
 
-export default class HelloWorld extends LightningElement {
+export default class ComputedValues extends LightningElement {
     @track greeting = 'World';
 
     get greetingUpper() {
--- src/modules/my/conditionals/conditionals.js
+++ src/modules/my/conditionals/conditionals.js
@@ -1,6 +1,6 @@
 import { LightningElement, track } from 'lwc';
 
-export default class HelloWorld extends LightningElement {
+export default class Conditionals extends LightningElement {
     @track showing = true;
 
     handleShowChange() {
